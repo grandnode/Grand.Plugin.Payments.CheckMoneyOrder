55131c8c8df0e21f59e95e869ca6e9c53170a6df
PatrykPorabik <patryk@porabik.pl>
2020-09-16 13:39:30 +0200

    init commit

---
+++ Models/PaymentInfoModel.cs
@@ -0,0 +1,9 @@
+﻿using Grand.Core.Models;
+
+namespace Grand.Plugin.Payments.CheckMoneyOrder.Models
+{
+    public class PaymentInfoModel : BaseModel
+    {
+        public string DescriptionText { get; set; }
+    }
+}
